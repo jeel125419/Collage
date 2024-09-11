f3dd8b8cda52ea84bfe3b21dd3055248b1409b93
jeel125419 <jeelmaheshwari2006@gmail.com>
2024-09-11 22:12:20 +0530

    TOH

--- towerOfHannoi.c
+++ towerOfHannoi.c
@@ -1,21 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+int count = 0;
 
-int towerOfHannoi(int no){
+int towerOfHannoi(int no, char s, char h, char d)
+{
+    count++;
     if (no == 1)
     {
+
+        printf("Transfer %d disc from %c to %c\n", no, s, d);
         return 1;
     }
-    else if (no < 1)
-    {
-        return -1;
-    }
-    
-
-    return (2*(towerOfHannoi(no - 1))) +1;  
+    towerOfHannoi(no - 1, s, d, h);
+    printf("Transfer %d disc from %c to %c\n", no, s, d);
+    towerOfHannoi(no - 1, h, s, d);
+    return 0;
 }
 
 int main()
 {
-    printf("%d",towerOfHannoi(4));
+    towerOfHannoi(3, 'S', 'H', 'D');
+    printf("%d", count);
     return 0;
 }
